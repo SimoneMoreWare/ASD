d09a09f6ef7bd9058453f4456779449330b59ebc
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-10-23 23:57:29 +0200

    Update E03.c

--- Laboratori/s2993369_2/L01/E03/E03.c
+++ Laboratori/s2993369_2/L01/E03/E03.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*typedef enum {
+typedef enum {
     zaffiro,rubino,topazio,smeraldo
-} Item;*/
+} Item;
 
-int disp_rip(int pos,int *val,int *sol,int n,int k,int *mark,int cnt);
+int disp_rip(int pos,int *mark,int *sol,int n,int k,int prec);
+int check(int prec,int att);
 
 int main(){
-    int val[4]={2,1,2,2};
-    int mark[4]={2,1,2,2};
+    int val[4]={19 ,6, 8, 5};
+    int mark[4]={19 ,6, 8, 5};
     int k=val[0]+val[1]+val[2]+val[3];
     int *sol;
     int n=4;
     int pos=0;
-    int cnt=0;
+    int i;
+    int prec=-1;
     sol=(int *) malloc(k*sizeof(int));
-    cnt= disp_rip(pos,val,sol,n,k,mark,cnt);
-    printf("%d",cnt);
+    for(i=k;i>=1 && cnt==0;i--) cnt= disp_rip(pos,mark,sol,n,i,cnt,prec);
+    printf("TEST \nzaffiro = %d, rubino = %d, topazio = %d, smeraldo = %d, TOT = %d\n",
+           val[zaffiro],
+           val[rubino],
+           val[topazio],
+           val[smeraldo],
+           k);
+    printf("Collana massima di lunghezza %d\n",i + 1);
     free(sol);
     return 0;
 }
 
-int disp_rip(int pos,int *val,int *sol,int n,int k,int *mark,int cnt){
+int disp_rip(int pos,int *mark,int *sol,int n,int k,int prec){
     int i;
     if (pos >= k) {//terminazione
         for (i=0; i<k; i++) printf("%d ", sol[i]);
         printf("\n");
-        return cnt+1;
+        return 1;
     }
     for (i = 0; i < n; i++) {//iterazioni sulle scelte
-        if (mark[i] > 0) {//mark non registra l uso o non l uso ma registra quante sono le sitanze disponibili da utilizzare, se c'è ne ancora una significa che la prendiamo
+        if (mark[i] > 0 && check(prec,i)) {//mark non registra l uso o non l uso ma registra quante sono le sitanze disponibili da utilizzare, se c'è ne ancora una significa che la prendiamo
             mark[i]--;//scelta decrementate
             sol[pos] = i;//scelta e poi vado sulla successiva
-            cnt = disp_rip(pos+1, val, sol, n, k,mark,cnt);//backtrack implicit
+            if(disp_rip(pos+1, mark, sol, n, k,i)) return 1;//backtrack implicit
             mark[i]++;//fase backtrack, annullo ultima scelta, rimetto in gioco quella scelta, incremento il vettore mark nella posizione i
         }
     }
-    return cnt;
+    return 0;
 }
 
+int check(int prec,int att){
+    if(prec==-1
+    || ((prec==zaffiro) && (att==zaffiro || att==rubino))
+    ||  ((prec==smeraldo) && (att==smeraldo || att==topazio))
+    ||  ((prec==rubino) && (att==smeraldo || att==topazio))
+    ||  ((prec==topazio) && (att==zaffiro || att==rubino))) return 1;
+    return 0;
+}
