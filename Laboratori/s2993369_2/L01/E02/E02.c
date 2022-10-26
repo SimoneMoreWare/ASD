5ae00ca3763a4c2f580b262fcf1ed509e98cd739
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-10-26 02:59:26 +0200

    finire cancellazione data

--- Laboratori/s2993369_2/L01/E02/E02.c
+++ Laboratori/s2993369_2/L01/E02/E02.c
@@ -16,7 +16,7 @@
  *
  * funzionalità
  * acquisizione ed inserimento ordinato di un nuovo elemento in lista da tastiera
- * acquisizione ed inserimento ordinato di nuovi elementi in lista da tastiera
+ * acquisizione ed inserimento ordinato di nuovi elementi in lista da file di testo
  * ricerca per codice
  * cancellazione con estrazione del dato di un elemento dalla lista previa ricerca per codice
  * cancellazione con estrazione del dato di tutti gli elementi con date comprese tra 2 date lette da testiera
@@ -32,10 +32,11 @@
 #include <string.h>
 
 #define MAXN 50
+#define MAXL 255
 #define maxncodice 7
 #define maxndatanascita 11
 
-#define filename "../brani.txt"
+typedef enum comando {r_leggidatastiera,r_leggidafile,r_ricercapercodice,r_cancellacodice,r_cancelladata,r_stampa,r_fine}comando_e;
 
 typedef struct{
     char codice[maxncodice];
@@ -53,23 +54,33 @@ struct anagraficanode {
     link next;
 };
 
-void leggifile();
+void menuparola(struct anagraficanode* head);
+comando_e leggicomando();
+
+void leggifile(struct anagraficanode** head);
 link SortListIns(struct anagraficanode* head, Item new_data);
 void list_display(struct anagraficanode *p);
 link newnode(struct anagraficanode* head,Item new_data);
 int keygreater(Item a,Item b);
 int datatoint(char *data);
+void inseriscielementodatastiera(struct anagraficanode** head);
+link ricercacodice(struct anagraficanode* head);
+link SortListDel(link h,link tmp);
 
 
 int main(){
-    leggifile();
+    struct anagraficanode* head=NULL;
+    menuparola(head);
     return 0;
 }
 
-void leggifile(){
-    struct anagraficanode* head=NULL;
+void leggifile(struct anagraficanode** head){
     Item dettagliaanagrafica;
     FILE *fpin;
+    char filename[MAXL];
+    fflush(stdin);
+    printf("Inserisci nome file");
+    scanf("%s",filename);
     fpin=fopen(filename,"r");
     if(fpin!=NULL){
         while(!feof(fpin)){
@@ -80,13 +91,77 @@ void leggifile(){
                    dettagliaanagrafica.via,
                    dettagliaanagrafica.citta,
                    &(dettagliaanagrafica.cap));
-            head=SortListIns(head,dettagliaanagrafica);
+            (*head)=SortListIns(*head,dettagliaanagrafica);
         }
-        list_display(head);
     }else{
         printf("Errore file");
     }
     fclose(fpin);
+}
+
+void menuparola(struct anagraficanode* head){
+    comando_e codicecomando;
+    struct anagraficanode* tmp;
+    int continua=1;
+
+    while(continua){
+        codicecomando=leggicomando();
+
+        switch (codicecomando){
+            case r_leggidatastiera:
+                inseriscielementodatastiera(&head);
+                list_display(head);
+                break;
+            case r_leggidafile:
+                leggifile(&head);
+                list_display(head);
+                break;
+            case r_ricercapercodice:
+                tmp=ricercacodice(head);
+                if(tmp!=NULL) printf("%s %s %s %s %s %s %d\n",tmp->val.codice,tmp->val.nome,tmp->val.cognome,tmp->val.datadinascita,tmp->val.via,tmp->val.citta,tmp->val.cap);
+                break;
+            case r_cancellacodice:
+                tmp= ricercacodice(head);
+                head=SortListDel(head,tmp);
+                list_display(head);
+                break;
+            case r_cancelladata:
+                printf("Inserire la funzione a livello di code 2");
+                break;
+            case r_stampa:
+                list_display(head);
+                break;
+            case r_fine:
+                printf("fine\n");
+                continua=0;
+                break;
+            default:
+                printf("comando errato");
+        }
+    }
+}
+
+comando_e leggicomando(){
+    comando_e c;
+    char cmd[MAXL];
+    char *tabella[r_fine+1]={
+            "0","1","2","3","4","5","6"
+    };
+    printf("inserisci comando\n"
+           "0 per inserire lista da tastiera\n"
+           "1 per inserire lista da file\n"
+           "2 per cercare codice\n"
+           "3 per cancella: cancella un record per codice\n"
+           "4 cancellaintervallo: cancella un intervallo di record compresi fra due date\n"
+           "5 stampa\n"
+           "6 per chiudere il programma\n");
+    fflush(stdin);
+    scanf("%s",cmd);
+    c=r_leggidatastiera;
+    while(c<r_fine && strcmp(cmd,tabella[c])!=0){
+        c++;
+    }
+    return c;
 }
 
 link newnode(struct anagraficanode* head,Item new_data){
@@ -126,6 +201,47 @@ int datatoint(char *data){
     }
     //trasformo tutto in secondi
     return res;
+}
+
+void inseriscielementodatastiera(struct anagraficanode** head){
+    Item dettagliaanagrafica;
+    printf("inserisci lista: ");
+    scanf("%s %s %s %s %s %s %d",dettagliaanagrafica.codice,
+                   dettagliaanagrafica.nome,
+                   dettagliaanagrafica.cognome,
+                   dettagliaanagrafica.datadinascita,
+                   dettagliaanagrafica.via,
+                   dettagliaanagrafica.citta,
+                   &(dettagliaanagrafica.cap));
+    (*head)=SortListIns(*head,dettagliaanagrafica);
+    //menuparola(*head);
+}
+
+link ricercacodice(struct anagraficanode* head){
+    char ricercato[MAXN];
+    //codice univoco, appena trovo un valore esco dal while
+    if(head==NULL){
+        printf("Non c'e' nessun elemento\n");
+        return NULL;
+    }
+    printf("Inserisci codice da ricercare\n");
+    fflush(stdin);
+    scanf("%s",ricercato);
+    while(head!=NULL){
+        if(strcmp(ricercato,head->val.codice)==0) return head;
+        head=head->next;
+    }
+    printf("Il codice da te inserito non e' presente in lista\n");
+    return NULL;
+}
+
+link SortListDel(link head, link tmp) {
+    if(head==NULL) return NULL;
+    if(tmp->next==NULL) head->next=NULL;
+    if(head==tmp) head=tmp->next;
+    else head->next=tmp->next;
+    free(tmp);
+    return head;
 }
 
 void list_display(struct anagraficanode *p){
