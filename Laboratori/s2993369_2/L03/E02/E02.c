c031c1a1f84c208ee6bc96bada6998356d21f06c
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-11-09 21:39:43 +0100

    Update E02.c

--- Laboratori/s2993369_2/L03/E02/E02.c
+++ Laboratori/s2993369_2/L03/E02/E02.c
@@ -8,15 +8,29 @@
 /*
  * calcolare lunghezza massima
  *
- * scrivere
- * fz
- * fr
- * ft
- * fs
- * scrivere matrice quadridimensionale Z R T S
- * scrivere MatrixMemorization
+ * Tecniche: divide et impera + memorization
  *
- * inizio con fz
+ * Implementazione
+ * Matrice quadridimensionale per salvare se ho gia trovato quella determinata configurazione di collana, altimenti -1
+ * Chiamo funzione Fz
+ * Decido quale gemma utilizzare per la posizione corrente
+ *      ricorro in base alle disponibilità delle gemme e delle regole
+ *          prima di ricorrere
+ *              si verifica se la soluzione è stata gia calcolando vedendo la matrice quadridimensioanle
+ *                  si ritorno valore in quella cella della matrice quadridimensionale
+ *                  no ricorsione sulla gemma successiva compatibile (es fz, chiamo prima fz e poi fr)
+ *                      chiamo la funzione con la gemma del tipo in esame decrementata
+ *                          verifico tra le due soluzioni parziali delle funzioni chiamate qual è la massima è la salva nella matrice quadridimensuionale
+ *                          backtrack, ritorno indietro e incremento la gemma di quel tipo in esame.
+ *                      * chiamo singolarmente prima fz, poi fr,fs,ft e verifico man mano qual è la lunghezza massima trovata dalle rispettive funzioni.
+ *      Condizione di terminazione
+ *          quando non sono piu disponibili gemme del tipo in esame
+ *      sottoproblema
+ *          collana con z,r,t,s ancora disponobili
+ *          soluzione
+ *              memorizzata in una cella quadrimensionale
+ *
+ *
  */
 
 
@@ -37,6 +51,7 @@ int fz(int ****mz,int ****mr,int ****mt,int ****ms,int *val);
 int fr(int ****mz,int ****mr,int ****mt,int ****ms,int *val);
 int ft(int ****mz,int ****mr,int ****mt,int ****ms,int *val);
 int fs(int ****mz,int ****mr,int ****mt,int ****ms,int *val);
+void dealloca(int ****memoizationZ,int ****memoizationR,int ****memoizationT,int ****memoizationS,int maxZ,int maxR,int maxT);
 int main(){
     FILE *fp;
     int lunghezza;
@@ -66,6 +81,8 @@ int SolveM(int *val){
     int MAXZ=val[zaffiro],MAXR=val[rubino],MAXT=val[topazio],MAXS=val[smeraldo];
     MemorizationZ=malloc4d(MAXZ,MAXR,MAXT,MAXS);
     MemorizationR=malloc4d(MAXZ,MAXR,MAXT,MAXS);
+    MemorizationT=malloc4d(MAXZ,MAXR,MAXT,MAXS);
+    MemorizationS=malloc4d(MAXZ,MAXR,MAXT,MAXS);
     //for(z=0;z<MAXZ;z++) for(r=0;r<MAXR;r++) for(t=0;t<MAXT;t++) for(s=0;s<MAXS;s++) printf("%d\n",MemorizationZ[z][r][t][s]=-1);
     lenmax=fz(MemorizationZ,MemorizationR,MemorizationT,MemorizationS,val);
     tmp=fr(MemorizationZ,MemorizationR,MemorizationT,MemorizationS,val);
@@ -74,8 +91,10 @@ int SolveM(int *val){
     lenmax=max(lenmax,tmp);
     tmp=fs(MemorizationZ,MemorizationR,MemorizationT,MemorizationS,val);
     lenmax=max(lenmax,tmp);
-    return lenmax+1;
+    dealloca(MemorizationZ,MemorizationR,MemorizationT,MemorizationS,MAXZ,MAXR,MAXT);
+    return lenmax;
     //inserisci free
+
 }
 
 int ****malloc4d(int MAXZ,int MAXR,int MAXT,int MAXS){
@@ -102,7 +121,7 @@ int fz(int ****mz,int ****mr,int ****mt,int ****ms, int *val){
     if(mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]!=-1) return mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]];
     val[zaffiro]--;
     maxz=fz(mz,mr,mt,ms,val);
-    maxr=fz(mz,mr,mt,ms,val);
+    maxr=fr(mz,mr,mt,ms,val);
     val[zaffiro]++;
     resz=max(maxz,maxr)+1;
     mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=resz;
@@ -113,13 +132,13 @@ int ft(int ****mz,int ****mr,int ****mt,int ****ms, int *val){
     int maxz,maxr;
     int rest;
     if(val[topazio]==0) return 0;
-    if(mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]!=-1) return mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]];
+    if(mt[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]!=-1) return mt[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]];
     val[topazio]--;
     maxz=fz(mz,mr,mt,ms,val);
     maxr=fr(mz,mr,mt,ms,val);
     val[topazio]++;
     rest=max(maxz,maxr)+1;
-    mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=rest;
+    mt[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=rest;
     return rest;
 }
 
@@ -133,7 +152,7 @@ int fr(int ****mz,int ****mr,int ****mt,int ****ms, int *val){
     maxt=ft(mz,mr,mt,ms,val);
     val[rubino]++;
     resr=max(maxs,maxt)+1;
-    mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=resr;
+    mr[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=resr;
     return resr;
 }
 
@@ -141,18 +160,44 @@ int fs(int ****mz,int ****mr,int ****mt,int ****ms, int *val){
     int maxs,maxt;
     int ress;
     if(val[smeraldo]==0) return 0;
-    if(mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]!=-1) return mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]];
+    if(ms[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]!=-1) return ms[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]];
     val[smeraldo]--;
     maxs=fs(mz,mr,mt,ms,val);
     maxt=ft(mz,mr,mt,ms,val);
     val[smeraldo]++;
     ress=max(maxs,maxt)+1;
-    mz[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=ress;
+    ms[val[zaffiro]][val[rubino]][val[topazio]][val[smeraldo]]=ress;
     return ress;
 }
 
 int max(int a,int b){
     return a>b ? a:b;
+}
+
+void dealloca(int ****memoizationZ,int ****memoizationR,int ****memoizationT,int ****memoizationS,int maxZ,int maxR,int maxT){
+    int i,j,k;
+    for(i = 0; i <= maxZ; i++) {
+        for(j = 0; j <= maxR; j++) {
+            for(k = 0; k <= maxT; k++) {
+                free(memoizationZ[i][j][k]);
+                free(memoizationR[i][j][k]);
+                free(memoizationT[i][j][k]);
+                free(memoizationS[i][j][k]);
+            }
+            free(memoizationZ[i][j]);
+            free(memoizationR[i][j]);
+            free(memoizationT[i][j]);
+            free(memoizationS[i][j]);
+        }
+        free(memoizationZ[i]);
+        free(memoizationR[i]);
+        free(memoizationT[i]);
+        free(memoizationS[i]);
+    }
+    free(memoizationZ);
+    free(memoizationR);
+    free(memoizationT);
+    free(memoizationS);
 }
 
 
