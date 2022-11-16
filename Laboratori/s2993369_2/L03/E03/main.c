03b7e6ea8dbad2c815659b7073f83e25b6ca8b87
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-11-16 21:50:54 +0100

    Update main.c

--- Laboratori/s2993369_2/L03/E03/main.c
+++ Laboratori/s2993369_2/L03/E03/main.c
@@ -83,10 +83,17 @@
 
 #define MAXL 50
 typedef enum comando{
-    r_leffiile,
+    r_leffiilepersonaggio,
     r_aggiungipersonaggio,
     r_eliminapersonaggio,
     r_ricercacodicepersonaggio,
+    r_leggifileinventario,
+    r_stampadettaglioggetto,
+    r_ricercanomeoggetto,
+    r_aggiungiequipaggiamento,
+    r_rimuoviequipaggiamento,
+    r_calcolostat,
+    r_stampadettaglipersonaggio,
     r_fine
 } comando_e;
 
@@ -102,6 +109,8 @@ void menuparola(void){
     comando_e codicecomando;
     int continua=1;
     tabPg *tabp;
+    int indexsearchname;
+    tabInv_t *tabi=NULL;
     tabp=allocazioneinizialepersonaggio();
 
     while(continua){
@@ -110,27 +119,57 @@ void menuparola(void){
         //scanf("%s",riga);//resto della riga
 
         switch (codicecomando){
-            case r_leffiile:
+            case r_leffiilepersonaggio:
                 tabp=leggifilepersonaggi(tabp);
-                stampapersonaggi(tabp);
                 break;
             case r_aggiungipersonaggio:
                 tabp= leggitastierapersonaggio(tabp);
-                stampapersonaggi(tabp);
                 break;
             case r_eliminapersonaggio:
                 eliminapersonaggio(tabp);
-                stampapersonaggi(tabp);
                 break;
             case r_ricercacodicepersonaggio:
                 ricercacodicepersonaggio(tabp);
                 break;
+            case r_leggifileinventario:
+                tabi=leggifileinventario();
+                break;
+            case r_stampadettaglioggetto:
+                if(tabi==NULL) printf("inventario vuoto\n");
+                else stampainventario(tabi);
+                break;
+            case r_ricercanomeoggetto:
+                indexsearchname=ricercanomeoggetto(tabi);
+                stampaoggettotrovato(tabi,indexsearchname);
+                break;
+            case r_aggiungiequipaggiamento:
+                if(tabp!=NULL && tabi!=NULL){
+                    tabp=aggiungereequipaggiamento(tabp,tabi);
+                }else{
+                    printf("Devi caricare prima la lista dei personaggi e l'inventario\n");
+                }
+                break;
+            case r_rimuoviequipaggiamento:
+                if(tabp!=NULL && tabi!=NULL){
+                    tabp=rimuovereequipaggiamento(tabp,tabi);
+                }else{
+                    printf("Devi caricare prima la lista dei personaggi e l'inventario\n");
+                }
+                break;
+            case r_calcolostat:
+                calcolostat(tabp);
+                break;
+            case r_stampadettaglipersonaggio:
+                stampapersonaggi(tabp);
+                break;
             case r_fine:
+                //aggiungere le free
+                freeTabella(tabp);
                 printf("fine\n");
                 continua=0;
                 break;
             default:
-                printf("comando errato");
+                printf("comando errato\n");
         }
     }
 }
@@ -139,16 +178,23 @@ comando_e leggicomando(void){
     comando_e c;
     char cmd[MAXL];
     char *tabella[r_fine+1]={
-            "0","1","2","3"
+            "0","1","2","3","4","5","6","7","8","9","10"
     };
     printf("Inserisci comando\n"
            "0 per inserire personaggi da file\n"
            "1 per aggiungere un personaggio\n"
            "2 per eliminare un personaggio\n"
-           "3 per ricerca codice personaggio\n");
+           "3 per ricerca codice personaggio\n"
+           "4 per inserire inventario da file\n"
+           "5 per stampare dettagli oggetto\n"
+           "6 per ricercare oggetto per nome\n"
+           "7 per aggiungere oggetto nell'equipaggiamento di un personaggio\n"
+           "8 per rimuovere oggetto nell'equipaggiamento di un personaggio\n"
+           "9 per calcolare le stat di un personaggio\n"
+           "10 per stampare personaggio\n");
     fflush(stdin);
     scanf("%s",cmd);
-    c=r_leffiile;
+    c=r_leffiilepersonaggio;
     while(c<r_fine && strcmp(cmd,tabella[c])!=0) c++;
     return c;
 }
