9bb80ad2a696fb8a1bbd457c6ec042d7f046df48
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-11-21 22:17:09 +0100

    Update E01.c

--- Laboratori/s293369_3/L07/E01/E01.c
+++ Laboratori/s293369_3/L07/E01/E01.c
@@ -1,293 +1,277 @@
-/*
- * corpo libero atleta deve:
- *      eseguire una sequenza di elementi
- *          eseguire una serie di passaggi acrobatici (diagonali)
- *              ogni diagonale è composta da uno o più elementi
- *              ogni elemento descritto da una serie di parametri
- *                  salvati in elementi.txt
- *                      numeri elementi riportato sulla prima riga del file
- *                  nome dell'elemento
- *                      stringa, max 100 char
- *                  tipologia
- *                      avanti 2
- *                      indietro 1
- *                      transizione 0
- *                  direzione di ingresso
- *                      entrare frontalmente 1
- *                      entrare di spalle 0
- *                  direzione di uscita
- *                      uscire frontalmente 1
- *                      uscire di spalle 0
- *                  requisito di preccdenza
- *                      elemento eseguito come primo di una sequenza 0
- *                      essere preceduto da un altro elemento 1
- *                  finale
- *                      elemento non può essere eseguito da altri elementi 1
- *                      o meno 0
- *                  valore
- *                      float
- *                      punteggio ottenuto dall'atleta per corettezza esecuzione di un elemento
- *                  difficolta
- *                      int
- *                      difficoltà esecuzione elemento
- *
- *                 due elementi possono essere eseguiti in sequenza se
- *                      la direzione di uscita del primo elemento == direzione di ingresso del secondo elemento
- *
- *                 un atleta inizia una diagonale sempre frontalmente
- *                      direzionediingresso[0]=1
- *
- *                 difficolta diagonale
- *                  somma difficoltà degli elementi che la compongono
- *
- *                difficolta gara
- *                  somma difficolta diagonali
- *
- *
- *                regole
- *                  3 diagonali
- *                  almeno un elemento acrobatico in ogni diagonale
- *                  nel programma ginnasta deve includere almeno un elemento acrobatico avanti e un elemento acrobatico indietro
- *                  il ginnasta deve presentare almeno una diagonale in cui compaiono almeno due elementi acrobatici in sequenza
- *                  se il ginnasta include un elemento finale di difficoltà 8 o superiore punteggio nell'ultima diagonale, il punteggio viene moltiplicato per 1.5
- *                  ogni diagonale al max 5 elementi
- *                  difficoltà diagonale <= DD
- *                  difficoltà programma <= DP
- */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAXN 100
-#define MAXELEMENTI 5
-#define filename "../elementi.txt"
+#define MAXE    5
+#define MAXDIAG 3
+#define DIFFICULT_EXTREME 8
 
-int cnt=0;
-int MAXDIAG=1000000;
+typedef enum {transizione, indietro, avanti} tipologia;
+typedef enum {spalle, fronte} direzione;
+typedef enum {no_seguito, seguito} precendenza;
+typedef enum {FALSE, TRUE} finale;
 
-/*typedef enum{
-    transizione,indietro,avanti
-}movimento;
+typedef struct {
+    char * nome;
+    tipologia tipo;
+    direzione ingresso, uscita;
+    precendenza prec;
+    finale fin;
+    float val;
+    int difficolta;
+} element;
+typedef struct {
+    int indexElement[MAXE];
+    int n;
+    float val;
+    int difficolta;
+} diagonal;
 
-typedef enum{
-    spalle,frontale
-}posizione;
+int leggifile(FILE *fp, element ** p_elemento);
+void elementRead(FILE *fp, element * elemento);
+void elementFree(element *elemento);
+void listaFree(element * elementi, int n);
 
-typedef enum{
-    primo,preceduto
-}requisito;
+int generazione(diagonal  ** p_diag, element * elemento, int n_el, int DD, int * p_ndiagMAX);
+void reallocazione_totale(diagonal ** p_diag, int * p_ndiagMAX);
+void disp_rip(element * elemento, diagonal ** p_diag, diagonal * p_diagTMP, int * p_ndiagMAX, int * p_n_actual, int n_el, int DD, int pos);
+int compatible_diagonal(diagonal * p_diagTMP, int pos, element * elemento, int i, int DD);
+int see_acrobatic( diagonal * p_diagTMP, element * elemento, int j);
+void find_sol(diagonal * diag, element * elemento, int ndiagEFF, int DP);
+void powerset_r(diagonal **b_diag, diagonal **tmp_diag, int j, diagonal *diag, element * elemento,
+                int ndiagEFF, int DP, int pos, float *p_b_val);
+int check_sol_prob(diagonal ** tmp_diag, element * elemento, int N, int DP);
+float calculate_val(diagonal ** tmp_diag, element * elemento, int N);
+void stampa_elemento(element * elemento);
+void stampadiag(diagonal * diag, element  * elemento);
 
-typedef enum{
-    seguito,nonseguito
-}azione;*/
-
-typedef struct{
-    char nome[MAXN];
-    int tipologia;
-    int direzionediingresso;
-    int direzionediuscita;
-    int requisitodiprecedenza;
-    int finale;
-    float valore;
-    int diffcolta;
-}elemento;
-
-typedef struct{
-    elemento elementi[MAXELEMENTI];
-    int num_elementi_utilizzati;
-}diagonale;
-
-elemento* leggifile(int *dim);
-void leggivalori(elemento **res,FILE *fp, int i);
-void powerset(elemento *elementi,int dim, diagonale *diagonali);
-void powerset_r(elemento* elementi, int dim, elemento* sol, int j, int pos,int start,int dd,int dp,diagonale** diagonali,int finale,int direzione);
-int check(elemento* elementi, int i, int dd,int pos,elemento* sol,int direzione);
-void combinazioniripetute(int pos,diagonale *diagonali,diagonale *sol_d,int k,int start,int dd,int dp,double *maxscore,diagonale **risultato);
-int checkprogramma(int pos,diagonale *diagonali,int i,int dp,diagonale *sol_d);
-int validate(diagonale *sol_d,int k);
-double calcolascore(diagonale *sol_d,int k);
-int main(){
-    elemento *elementi;
-    int dim;
-    diagonale *diagonali=NULL;
-    elementi=leggifile(&dim);
-    powerset(elementi,dim,diagonali);
-    printf("\n%d",cnt);
-    return 0;
-}
-
-elemento* leggifile(int *dim){
+int main() {
+    element * elemento;
+    int n, ndiagMAX=MAXDIAG, ndiagEFF, DD, DP;
+    char nomefile[100];
     FILE *fp;
-    elemento *res;
+    diagonal * diag;
+
+    printf("Inserisci nome del file:\t");
+    scanf("%s", nomefile);
+
+    if((fp=fopen(nomefile,"r"))==NULL)
+        return 1;
+
+    n=leggifile(fp, &elemento);
+    fclose(fp);
+
+    printf("Inserisci valore DD:\t");
+    scanf("%d", &DD);
+
+    printf("Inserisci valore DP:\t");
+    scanf("%d", &DP);
+
+    ndiagEFF= generazione(&diag, elemento, n, DD, &ndiagMAX);
+
+    printf("Le diagonali totali sono %d, ne ho allocate %d", ndiagEFF, ndiagMAX);
+
+    find_sol(diag, elemento, ndiagEFF, DP);
+
+    listaFree(elemento, n);
+    free(diag);
+
+    return 0;
+}
+
+int leggifile(FILE * fp, element ** p_elemento){
+    int i, n;
+    element * elementTMP;
+
+    fscanf(fp,"%d",&n);
+
+    elementTMP=malloc(n * sizeof(element));
+
+    for(i=0; i<n; i++)
+        elementRead(fp, &elementTMP[i]);
+
+    *p_elemento=elementTMP;
+    return n;
+}
+void elementRead(FILE *fp, element * elemento){
+    char stringaTMP[100];
+
+    fscanf(fp, "%s ", stringaTMP);
+    elemento->nome=strdup(stringaTMP);
+
+    fscanf(fp, "%d %d %d %d %d %f %d", &elemento->tipo, &elemento->ingresso, &elemento->uscita, &elemento->prec, &elemento->fin, &elemento->val, &elemento->difficolta);
+}
+void elementFree(element *elemento){
+    free(elemento->nome);
+}
+void listaFree(element * elementi, int n){
     int i;
-    fp= fopen(filename,"r");
-    if(fp!=NULL){
-        fscanf(fp,"%d\n",&(*dim));
-        res=(elemento *) malloc((*dim)*sizeof(elemento));
-        for(i=0;i<(*dim);i++) leggivalori(&res,fp,i);
-        return res;
-    }else{
-        printf("Errore file");
-        return NULL;
-    }
+    for(i=0; i<n; i++)
+        elementFree(&elementi[i]);
+
+    free(elementi);
 }
-
-void leggivalori(elemento **res,FILE *fp,int i){
-    fscanf(fp,"%s %d %d %d %d %d %f %d",
-           ((*res)[i]).nome,
-           &((*res)[i]).tipologia,
-           &((*res)[i]).direzionediingresso,
-           &((*res)[i]).direzionediuscita,
-           &((*res)[i]).requisitodiprecedenza,
-           &((*res)[i]).finale,
-           &((*res)[i]).valore,
-           &((*res)[i]).diffcolta
-    );
-}
-
-void powerset(elemento *elementi,int dim, diagonale *diagonali){
-    int j;
-    int maxelementi=5; //dato fornito dalla traccia
-    elemento *sol_e;
-    diagonale *sol_d;
-    diagonale *risultato;
-    double maxscore=0.0;
-    int dd=7,dp=30;
-    int pos=0;
-    int k;
-    int start=0;
-    if(dim<maxelementi) maxelementi=dim;
-    sol_e=(elemento *) malloc((maxelementi)*sizeof(elemento));
-    diagonali=(diagonale *) malloc(MAXDIAG*sizeof(diagonale));
-    for(j=1;j<=maxelementi;j++) powerset_r(elementi,dim,sol_e,j,pos,start,dd,dp,&diagonali,0,1);
-    for(int i=0;i<cnt;i++){
-        for(j=0;j<diagonali[i].num_elementi_utilizzati;j++){
-            printf("%s ",diagonali[i].elementi[j].nome);
-        }
-        printf("\n");
-    }
-    pos=0;
-    k=3;
-    start=0;
-    sol_d=(diagonale *) malloc(k*sizeof(diagonale));
-    risultato=(diagonale *) malloc(k*sizeof(diagonale));
-    combinazioniripetute(pos,diagonali,sol_d,k,start,dd,dp,&maxscore,&risultato);
-    printf("%0.3lf\n",maxscore);
-    /*for(int i=0;i<k;i++){
-        for(int h=0;h<risultato[i].num_elementi_utilizzati;h++){
-            printf("%s ",risultato[i].elementi[h].nome);
-        }
-        printf("\n---\n");
-    }*/
-
-}
-
-//disp ripetute
-void powerset_r(elemento* elementi, int dim, elemento* sol, int j, int pos,int start,int dd,int dp,diagonale** diagonali,int finale,int direzione){
+void stampadiag(diagonal * diag, element  * elemento){
     int i;
-    int tmp=0;
-    if(pos>=j || finale==1){
-        //printf("{ ")
-        for(i=0;i<j;i++){
-            if(cnt<MAXDIAG){
-                (*diagonali)[cnt].num_elementi_utilizzati=pos;
-                (*diagonali)[cnt].elementi[i]=sol[i];
-                tmp=tmp+sol[i].diffcolta;
-               // (*diagonali)[cnt]=sol[i];
-               //printf("Manda");
-            }else{
-                //ci sono piu condiserazione da fare
-                (*diagonali)=realloc((*diagonali),(2*MAXDIAG)*sizeof(elemento ***));
-                MAXDIAG=2*MAXDIAG;
-            }
-        }
-        printf("%d\n",tmp);
-        cnt++;
+    for(i=0; i< diag->n; i++)
+        stampa_elemento(&elemento[diag->indexElement[i]]);
+    printf("\n");
+}
+void stampa_elemento(element * elemento){
+    printf("%s %.3f\t",elemento->nome, elemento->val);
+}
+void reallocazione_totale(diagonal ** p_diag, int * p_ndiagMAX){
+    *p_diag=realloc( *p_diag, (*p_ndiagMAX)*2 * sizeof(diagonal) );
+    *p_ndiagMAX=(*p_ndiagMAX)*2;
+}
+
+
+int generazione(diagonal  ** p_diag, element * elemento, int n_el, int DD, int * p_ndiagMAX){
+    int n_actual=0;
+
+    diagonal * diag=    malloc((*p_ndiagMAX) * sizeof(diagonal));
+    diagonal diagTMP;
+
+    diagTMP.difficolta=0;
+    diagTMP.val=0;
+    disp_rip(elemento, &diag, &diagTMP, p_ndiagMAX, &n_actual, n_el, DD, 0);
+
+    *p_diag=diag;
+    return n_actual;
+}
+
+void disp_rip(element * elemento, diagonal ** p_diag, diagonal * p_diagTMP, int * p_ndiagMAX, int * p_n_actual, int n_el, int DD, int pos){
+    int i;
+
+    if(see_acrobatic(p_diagTMP, elemento, pos)) {
+        if (*p_n_actual == (*p_ndiagMAX - 1))
+            reallocazione_totale(p_diag, p_ndiagMAX);
+
+        p_diagTMP->n=pos;
+
+        (*p_diag)[(*p_n_actual)] = *p_diagTMP;
+
+        (*p_n_actual)++;
+    }
+
+    if(pos==MAXE)
         return;
-    }
-    for(i=start;i<dim;i++) {
-        if (check(elementi,i,dd,pos,sol,direzione)) {
-            sol[pos] = elementi[i];
-            powerset_r(elementi, dim, sol, j, pos + 1, i + 1, dd,dp,diagonali,elementi[i].finale,elementi[i].direzionediuscita);
-            //start++;
+
+    for(i=0; i<n_el; i++)
+        if(compatible_diagonal(p_diagTMP, pos, elemento, i, DD)) {
+            p_diagTMP->indexElement[pos]=i;
+
+            p_diagTMP->difficolta+=elemento[i].difficolta;
+            p_diagTMP->val+=elemento[i].val;
+
+            disp_rip(elemento, p_diag, p_diagTMP, p_ndiagMAX, p_n_actual, n_el, DD, pos + 1);
+
+            p_diagTMP->difficolta-=elemento[i].difficolta;
+            p_diagTMP->val-=elemento[i].val;
         }
-    }
 }
+int compatible_diagonal(diagonal * p_diagTMP, int pos, element * elemento, int i, int DD){
+    if(pos==0) {
+        if (elemento[i].prec == no_seguito && elemento[i].ingresso == fronte && elemento[i].difficolta <= DD)
+            return 1;
+        return 0;
+    }
 
+    if( elemento[ p_diagTMP->indexElement[pos-1] ].fin==FALSE &&
+        elemento[ p_diagTMP->indexElement[pos-1] ].uscita == elemento[i].ingresso  &&
+        (elemento[i].difficolta + p_diagTMP->difficolta) <=DD)
+        return 1;
 
-int check(elemento* elementi, int i, int dd,int pos,elemento* sol,int direzione){
-    int ddpartial=0;
-    int j;
-    if(pos!=0 && sol[0].direzionediingresso!=0) return 0; //direzionediingressofrontale
-    if(pos!=0 && sol[0].requisitodiprecedenza!=0) return 0; //direzionediingressofrontale
-    for(j=0;j<pos;j++) ddpartial=ddpartial+sol[j].diffcolta;
-    if((elementi[i].diffcolta+ddpartial)<=dd) return 1; //somma elementi < dd
     return 0;
 }
+int see_acrobatic( diagonal * p_diagTMP, element * elemento, int j){
+    int i;
 
-void combinazioniripetute(int pos,diagonale *diagonali,diagonale *sol_d,int k,int start,int dd,int dp,double *maxscore,diagonale **risultato){
-    int i,j;
-    int dpscore=0;
-    double score;
-    if(pos>=k){
-        if(validate(sol_d,k)) {
-            score=calcolascore(sol_d,k);
-            if(score>(*maxscore)){
-                (*maxscore)=score;
-                for(j=0;j<k;j++) (*risultato)[j]=sol_d[j];
+    for(i=0; i<j; i++)
+        if(elemento[p_diagTMP->indexElement[i]].tipo==indietro ||
+           elemento[p_diagTMP->indexElement[i]].tipo== avanti)
+            return 1;
+
+    return 0;
+}
+void find_sol(diagonal * diag, element * elemento, int ndiagEFF, int DP){
+    diagonal * b_diag[MAXDIAG];
+    diagonal * tmp_diag[MAXDIAG];
+    float b_val=0;
+    int i;
+
+    powerset_r(b_diag, tmp_diag, MAXDIAG, diag, elemento, ndiagEFF, DP, 0, &b_val);
+
+    printf("\n\nSTAMPA PROGRAMMA CON VALORE:\t%.3f\n", b_val);
+    for(i=0; i<MAXDIAG; i++)
+        stampadiag(b_diag[i], elemento);
+}
+void powerset_r(diagonal **b_diag, diagonal **tmp_diag, int j, diagonal *diag, element * elemento,
+                int ndiagEFF, int DP, int pos, float *p_b_val){
+    int i;
+    float val;
+
+    if(pos==j){
+        if(check_sol_prob(tmp_diag, elemento, j, DP)) {
+            val = calculate_val(tmp_diag, elemento, j);
+            if (*p_b_val < val) {
+                *p_b_val = val;
+                printf("\nBest %f", *p_b_val);
+                //questa serve per scaricare il buffer di stdout, altrimenti sembra che ci stia un casino
+                //a calcolare le diagonali, quando in realtà impiega tantissimo tempo per il calcolo del programma finale (3 diag)
+
+                for (i = 0; i < j; i++)
+                    b_diag[i] = tmp_diag[i];
             }
+        }
+        return ;
+    }
 
-           /* for (i = 0; i < k; i++) {
-                for (j = 0; j < sol_d[i].num_elementi_utilizzati; j++) {
-                    dpscore = dpscore + sol_d[i].elementi[j].diffcolta;
-                    printf("%s ", sol_d[i].elementi[j].nome);
-                }
-                printf("| ");
+    for(i=0; i<ndiagEFF; i++){
+        tmp_diag[pos]=&diag[i];
+        powerset_r(b_diag, tmp_diag, j, diag, elemento, ndiagEFF, DP, pos+1,  p_b_val);
+    }
+}
+int check_sol_prob(diagonal ** tmp_diag, element * elemento, int N, int DP) {
+    int check_2_seq_acrob=0, check_avanti=0, check_indietro=0, i, j, totalDifficult=0;
+
+    for(i=0; i< N; i++)
+        totalDifficult+=tmp_diag[i]->difficolta;
+
+    if(totalDifficult>DP)
+        return 0;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < tmp_diag[i]->n; j++) {
+            if (elemento[tmp_diag[i]->indexElement[j]].tipo != transizione){
+
+                if(check_avanti!=1 && elemento[tmp_diag[i]->indexElement[j]].tipo== avanti)
+                    check_avanti=1;
+                else if(check_indietro!=1)
+                    check_indietro=1;
+
+                if( check_2_seq_acrob!=1 &&
+                    (j < (tmp_diag[i]->n - 1)) &&
+                    elemento[tmp_diag[i]->indexElement[j+1]].tipo != transizione)
+                    check_2_seq_acrob=1;
+
+                if(check_avanti==1 && check_indietro==1 && check_2_seq_acrob==1)
+                    return 1;
             }
-            printf("DPeff %d", dpscore);
-            printf("\n-----\n");*/
-        }
-        return;
-    }
-    for(i=start;i<cnt;i++){
-        if(checkprogramma(pos,diagonali,i,dp,sol_d)){
-            sol_d[pos]=diagonali[i];
-            combinazioniripetute(pos+1,diagonali,sol_d,k,start,dd,dp,maxscore,risultato);
-            start++;
         }
     }
-}
-
-int checkprogramma(int pos,diagonale *diagonali,int i,int dp,diagonale *sol_d){
-    int dppartial=0;
-    int j,u;
-    for(j=0;j<pos;j++) for(u=0;u<sol_d[j].num_elementi_utilizzati;u++) dppartial=dppartial+sol_d[j].elementi[u].diffcolta;
-    for(u=0;u<diagonali[i].num_elementi_utilizzati;u++) dppartial=dppartial+diagonali[i].elementi[u].diffcolta;
-    if(dppartial<=dp) return 1;
     return 0;
 }
+float calculate_val(diagonal ** tmp_diag, element * elemento, int N){
+    int i;
+    float val=0;
 
-int validate(diagonale *sol_d,int k){
-    int checkavanti=0,checkindietro=0;
-    int checksequenza=0;
-    int i,j;
-    for (i = 0; i < k; i++) {
-        for (j = 0; j < sol_d[i].num_elementi_utilizzati; j++) {
-            if(sol_d[i].elementi[j].tipologia==1 && checkindietro!=1) checkindietro=1;
-            if(sol_d[i].elementi[j].tipologia==2 && checkavanti!=1) checkavanti=1;
-            if(j!=(sol_d[i].num_elementi_utilizzati-1) && sol_d[i].elementi[j].direzionediuscita==sol_d[i].elementi[j+1].direzionediingresso) checksequenza=1;
-        }
-    }
-    if(checkavanti==1 && checkindietro==1 && checksequenza==1) return 1;
-    return 0;
-}
+    for(i=0; i<N-1; i++)
+        val+=tmp_diag[i]->val;
 
-double calcolascore(diagonale *sol_d,int k){
-    int i,j;
-    double res=0;
-    for (i = 0; i < k; i++) {
-        for (j = 0; j < sol_d[i].num_elementi_utilizzati; j++) {
-            res=res+sol_d[i].elementi[j].valore;
-        }
-    }
-    return res;
+    if(elemento[tmp_diag[i]->indexElement[ (tmp_diag[i]->n) -1 ]].difficolta>= DIFFICULT_EXTREME)
+        val+= 1.5 * (tmp_diag[i]->val);
+    else val+= tmp_diag[i]->val;
+
+    return val;
 }
