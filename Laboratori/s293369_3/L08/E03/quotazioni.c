cc7d76d46e8935bf9e7c3637c047fce12eb972d7
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-12-09 15:23:50 +0100

    Create quotazioni.c

---
+++ Laboratori/s293369_3/L08/E03/quotazioni.c
@@ -0,0 +1,76 @@
+//
+// Created by simone on 08/12/22.
+//
+
+#include "quotazioni.h"
+
+typedef struct BSTnode* link;
+struct BSTnode { Quotazione item; link p; link l; link  r; int N; } ;
+struct binarysearchtree { link root;  link z; };
+static link NEW(Quotazione item, link p, link l, link r, int N);
+static void treePrintR(link h, link z);
+
+void readbst(BST bstq,FILE *fp){
+    Quotazione X;
+    bstq=BSTinit();
+    while(!feof(fp)){
+        X=QuotazioneScan(fp);
+        BSTinsert_leafI(bstq,X);
+    }
+    treePrintR(bstq->root,bstq->z);
+}
+
+void BSTinsert_leafI(BST bst, Quotazione x) {
+    link p = bst->root, h = p;
+    if (bst->root == bst->z) {
+        bst->root = NEW(x, bst->z, bst->z, bst->z, 1);
+        return;
+    }
+    while (h != bst->z) {
+        p = h;
+        h->N++;
+        h = (KEYcmp(KEYget(x.data), KEYget(h->item.data))==-1) ? h->l : h->r;
+    }
+    h = NEW(x, p, bst->z, bst->z, 1);
+    if (KEYcmp(KEYget(x.data), KEYget(p->item.data))==-1)
+        p->l = h;
+    else
+        p->r = h;
+}
+
+Quotazione QuotazioneScan(FILE *fp){
+    Quotazione val;
+    char stringadata[11];
+    fscanf(fp,"%s %*s %f %d",stringadata,&(val.valore),&(val.quantitatransiozioni));
+    sscanf (stringadata,"%d/%d/%d", &val.data.anno,&val.data.mese,&val.data.giorno);
+    return val;
+}
+
+BST BSTinit() {
+    BST bst = malloc(sizeof *bst) ;
+    bst->root = ( bst->z = NEW(ITEMsetNull(), NULL, NULL, NULL, 0));
+    return bst;
+}
+
+Quotazione ITEMsetNull() {
+    Quotazione val = {0,0,0,0,0};
+    return val;
+}
+
+static link NEW(Quotazione item, link p, link l, link r, int N) {
+    link x = malloc(sizeof *x);
+    x->item = item; x->p = p; x->l = l; x->r = r; x->N = N;
+    return x;
+}
+
+void QuotationStore(Quotazione val) {
+    printf("year: %d month: %d day: %d value: %.2f number: %d\n", val.data.anno, val.data.mese,val.data.giorno,val.valore,val.quantitatransiozioni);
+}
+
+static void treePrintR(link h, link z) {
+    if (h == z)
+        return;
+    treePrintR(h->l, z);
+    QuotationStore(h->item);
+    treePrintR(h->r, z);
+}
