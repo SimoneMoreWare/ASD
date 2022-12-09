dc6fc7e2f6a596e0bb35708cddeda90756cce74c
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-12-09 15:24:27 +0100

    Create titles.h

---
+++ Laboratori/s293369_3/L08/E03/titles.h
@@ -0,0 +1,18 @@
+//
+// Created by simone on 08/12/22.
+//
+
+#ifndef UNTITLED16_TITLES_H
+#define UNTITLED16_TITLES_H
+
+#define MAXC 21
+#include "datetime.h"
+#include <stdio.h>
+#include "quotazioni.h"
+
+typedef struct title *Title;
+typedef struct listtitle *LISTTITLE;
+
+void readfile(FILE *fp);
+
+#endif //UNTITLED16_TITLES_H
