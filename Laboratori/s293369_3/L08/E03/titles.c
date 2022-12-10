8d8438e15df1bea6287138fc812de9c4289b7cf7
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-12-10 01:44:37 +0100

    Update titles.c

--- Laboratori/s293369_3/L08/E03/titles.c
+++ Laboratori/s293369_3/L08/E03/titles.c
@@ -4,18 +4,127 @@
 
 #include "titles.h"
 
-struct{
+struct title{
     char name[MAXC];
-    datetime data;
     BST quotazioni;
-}title;
+};
 
 typedef struct node *link;
 struct node { Title val; link next; };
 struct listtitle { link head; int N; };
+static link SortListIns(LISTTITLE l, char new_data[],FILE *fp);
+static link newnode(link head,char new_data[], FILE *fp);
 
-void readfile(FILE *fp){
-    BST bstq = NULL;
-    readbst(bstq,fp);
+LISTTITLE readfile(FILE *fp,LISTTITLE l){
+    char nametitolo[MAXC];
+    int dimensionetitoli;
+    fscanf(fp,"%d\n",&dimensionetitoli);
+    for(int i=0;i<dimensionetitoli;i++){
+        fscanf(fp,"%s",nametitolo);
+        ((l)->head)= SortListIns(l,nametitolo,fp);
+        //printf("%s",l->head->val->name);
+    }
+    list_display(l);
+    return l;
+    //
 }
 
+LISTTITLE LISTinit() {
+    LISTTITLE T = malloc(sizeof(LISTTITLE));
+    T->head = NULL;
+    T->N = 0;
+    return T;
+}
+
+Title title_init() {
+    Title T = malloc(sizeof(Title));
+    T->quotazioni=BSTinit();
+    return T;
+}
+
+static link SortListIns(LISTTITLE l, char new_data[],FILE *fp){
+    link x,p;
+    if((l->head)==NULL || keygreater(l->head->val->name,new_data)>0){
+        l->N++;
+        return newnode(l->head,new_data,fp);
+    }
+    for(x=(l->head)->next,p=(l->head);
+        x!=NULL && keygreater(new_data,x->val->name)>0;
+        p=x,x=x->next);
+    p->next=newnode(x,new_data,fp);
+    l->N++;
+    return l->head;
+}
+
+int keygreater(char *a,char *b){
+    return strcmp(a,b);
+}
+
+link newnode(link head,char new_data[],FILE *fp){
+    struct node* nuovo = (struct node*) malloc(sizeof(struct node));
+    nuovo->val = title_init(); //DEVI RICORDATI DI INIZIALIZZARE IL TITOLO ALTRIMENTI CRASHA TUTTO1
+    //malloc non andata a buon fine
+    if(nuovo==NULL){
+        printf("Errore nell'allocazione della memoria");
+        exit(0);
+    }
+    //ho un puntatore temporaneo nuovo, uscito dalla funzione viene liberato1
+    strcpy(nuovo->val->name,new_data);
+    nuovo->next=(head);//uguale alla vecchia testa
+    readbst(&(nuovo->val->quotazioni),fp);
+
+    return nuovo;
+}
+
+
+
+void list_display(LISTTITLE l){
+    printf("\n");
+    link tmp=l->head;
+    if(tmp==NULL) printf("Non c'è la lista\n");
+    while((tmp)!=NULL){
+        printf("%s\n",(tmp->val->name));
+        treePrintWrapper(tmp->val->quotazioni);
+        (tmp)=(tmp)->next;
+    }
+}
+
+void SearchTitle(LISTTITLE l){
+    char searchtitle[MAXC];
+    link tmp=l->head;
+
+    printf("Inserisci titolo da ricercare: ");
+    scanf("%s",searchtitle);
+
+    while(tmp!=NULL && keygreater(searchtitle,tmp->val->name)<=0){
+        if(strcmp(searchtitle,tmp->val->name)==0){
+            printf("Titolo trovato\n");
+            printf("%s\n",(tmp->val->name));
+            treePrintWrapper(tmp->val->quotazioni);
+            return;
+        }
+        (tmp)=(tmp)->next;
+    }
+    printf("Il titolo inserito non è presente nella lista\n");
+}
+
+void SearchTitleQuotationFromDate(LISTTITLE l){
+    char searchtitle[MAXC];
+    char date[11];
+    link tmp=l->head;
+
+    printf("Inserisci titolo da ricercare: ");
+    scanf("%s",searchtitle);
+
+    while(tmp!=NULL){
+        if(strcmp(searchtitle,tmp->val->name)==0 && keygreater(searchtitle,tmp->val->name)<=0){
+            printf("Titolo trovato\n");
+            printf("Inserisci data: ");
+            scanf("%s",date);
+            SearchBSTQuotationFromdate(tmp->val->quotazioni,date);
+            return;
+        }
+        (tmp)=(tmp)->next;
+    }
+    printf("Il titolo inserito non è presente nella lista\n");
+}
